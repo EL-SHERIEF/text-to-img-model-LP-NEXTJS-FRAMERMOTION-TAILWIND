6edcd92130334d88ded61c6e2c88a5b54cad9ba0
EL-SHERIEF <ahmed-elsherief@outlook.com>
2025-02-13 15:34:42 +0200

    black

--- app/components/FloatingMockups.jsx
+++ app/components/FloatingMockups.jsx
@@ -14,7 +14,7 @@ import photo7 from '@/public/Photos/8.jpeg';
 import photo8 from '@/public/Photos/9.jpeg';
 
 const photos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8];
-
+ 
 const elements = [
   { x: '-18%', y: '10%', width: 300, height: 300 },
   { x: '-6%', y: '45%', width: 100, height: 140 },
@@ -40,7 +40,7 @@ const FloatingMockups = () => {
  <>
     <motion.div
       ref={containerRef}
-      className="absolute z-0 top-5 w-screen h-[120vh] flex justify-center items-center overflow-hidden pointer-events-none"
+      className="opacity-40 absolute z-0 top-5 w-screen h-[120vh] flex justify-center items-center overflow-hidden pointer-events-none"
       style={{ y: parallaxY }} // Apply parallax movement
     >
       <div className="relative w-full h-full max-w-6xl mx-auto">
